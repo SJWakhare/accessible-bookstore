038376c3f207fbdaca33bfbb14cb4391d2204404
SJWakhare <wshivani123@gmail.com>
2026-03-18 21:19:35 +0530

    Components generated and routing added.

--- frontend/accessible-bookstore-ui/src/app/app.routes.ts
+++ frontend/accessible-bookstore-ui/src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./home/home.component').then(m => m.HomeComponent)
+  },
+  {
+    path: 'books',
+    loadComponent: () =>
+        import('./books/books.component').then(m =>m.BooksComponent)
+  },
+  {
+    path: 'book/:id',
+    loadComponent: ()=>
+        import('./book-detail/book-detail.component').then(m=> m.BookDetailComponent)
+  },
+  {
+    path: 'cart',
+    loadComponent: ()=>
+        import('./cart/cart.component').then(m => m.CartComponent)
+  }
+];
--- frontend/accessible-bookstore-ui/src/main.ts
+++ frontend/accessible-bookstore-ui/src/main.ts
@@ -1,6 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
 import { AppComponent } from './app/app.component';
+import { routes } from './app/app.routes';
 import 'zone.js';
 
-bootstrapApplication(AppComponent)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent,{
+  providers :[provideRouter(routes)]
+});
